6a8aebd91fac78e3793a342a23fd1a6cba276e87
melbourne333 <melbourne333@mail.ru>
2023-04-16 18:13:30 +1000

    0.0.2

--- src/main/java/org/example/King.java
+++ src/main/java/org/example/King.java
@@ -7,18 +7,43 @@ public class King extends ChessPiece{
 
     @Override
     public String getColor() {
-        return this.color;
+        return color;
     }
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        return ((0 <= toLine) && (toLine < 8) && (0 <= toColumn) && (toColumn < 8))
-                && ((Math.abs(line - toLine) + Math.abs(column - toColumn) == 1)
-                || (Math.abs(line - toLine) + Math.abs(column - toColumn) == 2))
-                && !((line == toLine) && (column == toColumn));
+        if (checkPos(line) && checkPos(column) && checkPos(toLine) &&checkPos(toColumn)){
+            if (Math.abs(line - toLine) > 1 || Math.abs(column - toColumn) > 1) return false;
+
+            if (isUnderAttack(chessBoard, toLine, toColumn)) return false;
+
+            if (chessBoard.board[toLine][toColumn] != null){
+                return  !chessBoard.board[toLine][toColumn].getColor().equals(color);
+            }
+
+            return true;
+        }return false;
     }
     @Override
     public String getSymbol() {
         return "K";
+    }
+
+    public boolean isUnderAttack(ChessBoard chessBoard, int line, int column){
+        if (checkPos(line) && checkPos(column)){
+            for (int i = 0; i < 7; i++){
+                 for (int j = 0; j < 7; j++){
+                     if (chessBoard.board[i][j] != null) {
+                         if (!chessBoard.board[i][j].getColor().equals(color) && chessBoard.board[i][j].canMoveToPosition(chessBoard, i, j, line, column)) {
+                             return true;
+                         }
+                     }
+                 }
+            }
+        }
+        return false;
+    }
+    public boolean checkPos(int pos) {
+        return pos >= 0 && pos <= 7;
     }
 }
